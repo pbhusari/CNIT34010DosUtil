8215ffcfcf71ce0ba792f9713789601ae0f82a78
pbhusari <pranavbhusari2013@gmail.com>
2019-11-16 16:17:59 -0500

    Documentation and error reporting

--- dosutil.sh
+++ dosutil.sh
@@ -4,10 +4,27 @@
 # This script converts certain windows commands to unix commands
 # November 17th. 2019
 
-# 0 --> File
-# 1 --> Directory
-# 2 --> DNE
+# Variables:
 
+# Global:
+# -------
+# USERCOMMAND 	--> Command that the user is inputting
+# USERARG1 		--> The first argument that the user is inputting
+# USERARG2 		--> The second argument that the user is inputting
+# SUCCSESSFUL	--> Stores if a command was succsessful or not
+
+# Verify:
+# -------
+# FILE 			--> file that the verification funciton is acting on
+
+# Command:
+# --------
+# COMMAND 		--> Command that the function is acting on
+# ARG1 			--> First argument that the function is acting on
+# ARG2 			--> Second argument that the function is acting on
+# TYPE 			--> Stores the type of the file to be used in case statments
+
+# "File verification function"
 verify() {
 	
 	FILE=$1
@@ -26,16 +43,21 @@ verify() {
 	if ! test -f "$FILE"; then
 		return 2
 	fi
+	
+	# 0 --> File
+	# 1 --> Directory
+	# 2 --> DNE
 
 }
 
-
+# "UNIX command execution function"
 command() {
 
 	COMMAND=$1
 	ARG1=$2
 	ARG2=$3
 
+	# "A case statement is required"
 	case $COMMAND in 
 		author)
 			echo "Bhusari Pranav"
@@ -52,11 +74,11 @@ command() {
 					fi
 					;;
 				1)
-					echo "This is a directory"
+					echo "ERROR: $ARG1 is a directory"
 					return 1
 					;;
 				2)
-					echo "File Does Not Exist"
+					echo "ERROR: $ARG1 Does Not Exist"
 					return 1
 					;;
 			esac
@@ -67,7 +89,8 @@ command() {
 			TYPE=$?
 			case $TYPE in
 				0)
-					echo "Cannot overwrite file: $ARG2"
+					# "Build in handling for file/directory overwrites"
+					echo "ERROR: Cannot overwrite file: $ARG2"
 					return 1
 					;;
 				1|2)
@@ -75,14 +98,14 @@ command() {
 					TYPE=$?
 					case $TYPE in
 						0)
-							if cp "$ARG1" "$ARG2"; then 
+							if cp "$ARG1" "$ARG2" > /dev/null; then 
 								return 0
 							else
 								return 1
 							fi	
 							;;
 						1)
-							if cp -r "$ARG1" "$ARG2"; then 
+							if cp -r "$ARG1" "$ARG2" > /dev/null; then 
 								return 0
 							else
 								return 1
@@ -90,7 +113,7 @@ command() {
 							;;
 						2)
 														
-							echo "File Does not exist"
+							echo "ERROR: $ARG1 Does not exist"
 							return 1
 							;;
 					esac
@@ -102,7 +125,7 @@ command() {
 			TYPE=$?
 			case $TYPE in
 				0|1)
-					echo "Name already Exists"
+					echo "ERROR: $ARG2 already Exists"
 					return 1
 					;;
 				2)		
@@ -110,14 +133,14 @@ command() {
 					TYPE=$?
 					case $TYPE in
 						0|1)
-							if mv "$ARG1" "$ARG2"; then
+							if mv "$ARG1" "$ARG2" > /dev/null; then
 								return 0
 							else
 								return 1
 							fi
 							;;
 						2)
-							echo "Source does not exist"
+							echo "ERROR: $ARG1 does not exist"
 							return 1
 							;;
 					esac
@@ -128,7 +151,8 @@ command() {
 			TYPE=$?
 			case $TYPE in 
 				0)
-					echo "Cannot overwrite file: $ARG2"
+					# "Build in handling for file/directory overwrites"
+					echo "ERROR: Cannot overwrite file: $ARG2"
 					return 1
 					;;
 				1|2)
@@ -136,14 +160,14 @@ command() {
 					TYPE=$?
 					case $TYPE in 
 						0|1)
-							if mv "$ARG1" "$ARG2"; then
+							if mv "$ARG1" "$ARG2" > /dev/null; then
 								return 0
 							else
 								return 1
 							fi
 						;;
 						2)
-							echo "Source does not exist"
+							echo "ERROR: $ARG1 does not exist"
 							return 1
 						;;
 					esac
@@ -172,7 +196,7 @@ command() {
 					fi
 					;;
 				2)
-					echo "File not found!"
+					echo "ERROR: $ARG1 does not exist"
 					;;
 
 			esac
@@ -192,9 +216,23 @@ command() {
 	
 }
 
+# "All parameters must be copied into descriptive variable names"
+USERCOMMAND=$1
+USERARG1=$2
+USERARG2=$3 
 
-command "$1" "$2" "$3"
-echo $?
+command "$USERCOMMAND" "$USERARG1" "$USERARG2"
+SUCCSESSFUL=$?
+
+# The verification for command ssuccses of the COMMAND funciton is the return value
+# This enables further expandability with other shell scipts
+if [ "$SUCCSESSFUL" != 1 ]; then
+	echo "Command executed succsessfully"
+else
+	echo "Command executed with errors"
+fi
+
+
 
 
 
