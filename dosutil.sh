1db8013471570a267edec4368bfef2022e69ee02
pranav <pranavbhusari2013@gmail.com>
2019-12-08 15:04:45 -0500

    Phase 2 Begin

--- dosutil.sh
+++ dosutil.sh
@@ -8,7 +8,6 @@
 
 # Global:
 # -------
-# USERCOMMAND 	--> Command that the user is inputting
 # USERARG1 		--> The first argument that the user is inputting
 # USERARG2 		--> The second argument that the user is inputting
 # SUCCSESSFUL	--> Stores if a command was succsessful or not
@@ -26,7 +25,7 @@
 
 # "File verification function"
 verify() {
-	
+
 	FILE=$1
 
 	# Test if something is a file
@@ -43,7 +42,7 @@ verify() {
 	if ! test -f "$FILE"; then
 		return 2
 	fi
-	
+
 	# 0 --> File
 	# 1 --> Directory
 	# 2 --> DNE
@@ -53,15 +52,16 @@ verify() {
 # "UNIX command execution function"
 command() {
 
-	COMMAND=$1
+	COMMAND=$(echo "$1" | awk '{print tolower($0)}')
 	ARG1=$2
 	ARG2=$3
 
 	# "A case statement is required"
-	case $COMMAND in 
+	case $COMMAND in
 		author)
 			echo "Bhusari Pranav"
 			;;
+
 		type)
 			verify "$ARG1"
 			TYPE=$?
@@ -74,8 +74,12 @@ command() {
 					fi
 					;;
 				1)
-					echo "ERROR: $ARG1 is a directory"
-					return 1
+          echo "$ARG1 is a Directory"
+					if echo "$(ls -l $ARG1)"; then
+					    return 0
+          else
+              return 1
+          fi
 					;;
 				2)
 					echo "ERROR: $ARG1 Does Not Exist"
@@ -83,7 +87,6 @@ command() {
 					;;
 			esac
 			;;
-
 		copy)
 			verify "$ARG2"
 			TYPE=$?
@@ -98,21 +101,21 @@ command() {
 					TYPE=$?
 					case $TYPE in
 						0)
-							if cp "$ARG1" "$ARG2" > /dev/null; then 
+							if cp "$ARG1" "$ARG2" > /dev/null; then
 								return 0
 							else
 								return 1
-							fi	
+							fi
 							;;
 						1)
-							if cp -r "$ARG1" "$ARG2" > /dev/null; then 
+							if cp -r "$ARG1" "$ARG2" > /dev/null; then
 								return 0
 							else
 								return 1
-							fi	
+							fi
 							;;
 						2)
-														
+
 							echo "ERROR: $ARG1 Does not exist"
 							return 1
 							;;
@@ -120,7 +123,7 @@ command() {
 					;;
 			esac
 			;;
-		ren)
+    ren)
 			verify "$ARG2"
 			TYPE=$?
 			case $TYPE in
@@ -128,7 +131,7 @@ command() {
 					echo "ERROR: $ARG2 already Exists"
 					return 1
 					;;
-				2)		
+				2)
 					verify "$ARG1"
 					TYPE=$?
 					case $TYPE in
@@ -149,7 +152,7 @@ command() {
 		move)
 			verify "$ARG2"
 			TYPE=$?
-			case $TYPE in 
+			case $TYPE in
 				0)
 					# "Build in handling for file/directory overwrites"
 					echo "ERROR: Cannot overwrite file: $ARG2"
@@ -158,7 +161,7 @@ command() {
 				1|2)
 					verify "$ARG1"
 					TYPE=$?
-					case $TYPE in 
+					case $TYPE in
 						0|1)
 							if mv "$ARG1" "$ARG2" > /dev/null; then
 								return 0
@@ -172,10 +175,9 @@ command() {
 						;;
 					esac
 					;;
-			
+
 			esac
 			;;
-            
 		del)
 
 			verify "$ARG1"
@@ -201,38 +203,179 @@ command() {
 
 			esac
 			;;
+		copy!)
+			verify "$ARG2"
+			TYPE=$?
+			case $TYPE in
+				0|1|2)
+					verify "$ARG1"
+					TYPE=$?
+					case $TYPE in
+						0)
+							if cp -f "$ARG1" "$ARG2" > /dev/null; then
+								return 0
+							else
+								return 1
+							fi
+					 		;;
+						1)
+							if cp -rf "$ARG1" "$ARG2" > /dev/null; then
+								return 0
+							else
+								return 1
+							fi
+							;;
+						2)
 
-		help|*)
+							echo "ERROR: $ARG1 Does not exist"
+							return 1
+							;;
+				esac
+				;;
+			esac
+			;;
+		ren!)
+			verify "$ARG2"
+			TYPE=$?
+			case $TYPE in
+				0|1)
+					echo "ERROR: $ARG2 already Exists"
+					return 1
+					;;
+				2)
+					verify "$ARG1"
+					TYPE=$?
+					case $TYPE in
+						0|1)
+							if mv -f "$ARG1" "$ARG2" > /dev/null; then
+								return 0
+							else
+								return 1
+							fi
+							;;
+						2)
+							echo "ERROR: $ARG1 does not exist"
+							return 1
+							;;
+					esac
+			esac
+			;;
+		move!)
+			verify "$ARG2"
+			TYPE=$?
+			case $TYPE in
+				0)
+					# "Build in handling for file/directory overwrites"
+					echo "ERROR: Cannot overwrite file: $ARG2"
+					return 1
+					;;
+				1|2)
+					verify "$ARG1"
+					TYPE=$?
+					case $TYPE in
+						0|1)
+							if mv -f "$ARG1" "$ARG2" > /dev/null; then
+								return 0
+							else
+								return 1
+							fi
+						;;
+						2)
+							echo "ERROR: $ARG1 does not exist"
+							return 1
+						;;
+					esac
+					;;
+
+			esac
+			;;
+    whereami)
+      pwd
+      ;;
+
+    cd)
+      verify "$ARG1"
+			TYPE=$?
+			case $TYPE in
+				0)
+					echo "ERROR: $ARG1 is a file"
+					;;
+				1)
+					if cd "$ARG1"; then
+							return 0
+					else
+						return 1
+					fi
+					;;
+				2)
+					echo "ERROR: $ARG1 does  not exist"
+					;;
+				esac
+				;;
+
+		exit)
+			echo "Program Exited"
+			;;
+    help|*)
 			echo "CNIT 34010 - Dosutil Phase 1"
 			echo "============================"
-			echo "author				output author's name in the form last, first"
-			echo "type [FILE]			output the content of the file in the first parameter"
+			echo "author				output author's name in the form last first"
+			echo "type [[ FILE]			output the content of the file in the first parameter"
 			echo "copy [SOURCE] [DESTINATION]	copy a file"
 			echo "ren [FILE] [NEWNAME]		rename a file"
 			echo "move [SOURCE] [DESTINATION]	move a file"
 			echo "del [FILE]  			delete a file"
-			echo "help  				display a list of commands"
-	esac	
-	
+			echo "help  					display a list of commands"
+      ;;
+
+
+	esac
+
 }
 
 # "All parameters must be copied into descriptive variable names"
-USERCOMMAND=$1
-USERARG1=$2
-USERARG2=$3 
 
-command "$USERCOMMAND" "$USERARG1" "$USERARG2"
-SUCCSESSFUL=$?
+if ! test -z $1 ; then
+	COMMAND=$(echo "$1" | awk '{print tolower($0)}')
+	USERARG1=$2
+	USERARG2=$3
 
-# The verification for command ssuccses of the COMMAND funciton is the return value
-# This enables further expandability with other shell scipts
-if [ "$SUCCSESSFUL" != 1 ]; then
-	echo "Command executed succsessfully"
+	command "$COMMAND" "$USERARG1" "$USERARG2"
+
+	if [ "$?" != "1" ]; then
+		echo "Command executed succsessfully"
+	else
+		echo "Command executed with errors"
+	fi
 else
-	echo "Command executed with errors"
+	select COMMAND in type copy ren del move help copy! move! ren! whereami cd sort author exit
+	do
+			# Commands that require 1+ args
+			if 	[[ $COMMAND = "type" ]] || [[ $COMMAND == "copy" ]] || [[ $COMMAND == "ren" ]] || [[ $COMMAND == "del" ]] ||  [[ $COMMAND == "move" ]] || [[ $COMMAND == "copy!" ]] || [[ $COMMAND == "move!" ]] || [[ $COMMAND == "ren!" ]] ||  [[ $COMMAND == "cd" ]]; then
+					select USERARG1 in $(ls)
+					do
+						if [[ $COMMAND == "ren" ]] || [[ $COMMAND == "copy" ]] || [[ $COMMAND == "move" ]] || [[ $COMMAND == "copy!" ]] || [[ $COMMAND == "move!" ]] || [[ $COMMAND == "ren!" ]]; then
+							read USERARG2
+						fi
+						break
+					done
+			fi
+
+			if [[ $COMMAND == "exit" ]]; then
+				break
+			fi
+
+			command "$COMMAND" "$USERARG1" "$USERARG2"
+			SUCCSESSFUL=$?
+			# The verification for command ssuccses of the COMMAND funciton is the return value
+			# This enables further expandability with other shell scipts
+			if [ "$SUCCSESSFUL" != "1" ]; then
+				echo "Command executed succsessfully"
+			else
+				echo "Command executed with errors"
+			fi
+
+	done
 fi
 
-
-
-
-
+exec bash
